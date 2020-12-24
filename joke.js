cf34afdd0587d4f7499df118ec4b6035065bcd94
mbilalbhattirajpoot <m.bilalbhattirajpoot@gmail.com>
2020-12-23 17:08:00 -0800

    #3 routes

--- joke.js
+++ joke.js
@@ -16,8 +16,10 @@
 const express=require ('express')
 const app = express()
 
-app.get('/',(req,res)=>{
-  respose.send("hello world ")
-  
-})
+app.get('/',(req, res) =>{
+  res.send("hello world ")
 
+})
+app.listen(3200,()=>{
+console.log ( "works!")
+})
