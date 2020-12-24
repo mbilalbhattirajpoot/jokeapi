56fa0407c55f2208cb9aa5144c1ea4845dc9a3af
mbilalbhattirajpoot <m.bilalbhattirajpoot@gmail.com>
2020-12-23 16:13:39 -0800

    #2 changes

--- joke.js
+++ joke.js
@@ -1,10 +1,23 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-  .then(response => {
-    console.log(response.data.url);
-    console.log(response.data.explanation);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+//   .then(response => {
+//     console.log(response.data.url);
+//     console.log(response.data.explanation);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//     console.log(language);
+//     console.log(category);
+//     console.log(flags.religious);
+//     console.log(flags.political);
+//     console.log(joketype);
+//   });
+const express=require ('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+  respose.send("hello world ")
+  
+})
+
